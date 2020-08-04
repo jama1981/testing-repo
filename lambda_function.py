5b4248fd669e71ab9d51dc1e0323db286c3010f4
jama1981 <zimp@inbox.ru>
2020-08-03 21:11:20 -0400

    Lambda_function

---
+++ lambda_function.py
@@ -0,0 +1,11 @@
+Import json
+
+def lambda_handler(event, context):
+    print(json.dumps(event))
+    return {
+        'status': 200,
+        'headers': {
+            'Content-Type': 'application/json'
+        },
+        'body': json.dumps({'message': 'Hello-OK'})
+    }
